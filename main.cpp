7b93d0be4c2f521c223e68358570593fb0b2bde6
Abdurahim Hudulov <abul.hudul@gmail.com>
2023-12-06 07:30:48 +0100

    customized it and removed unnecessary file

--- main.cpp
+++ main.cpp
@@ -1,13 +1,18 @@
-#include <iostream>
 #include <vector>
-#include <string>
 #include <algorithm>
-#include <random>
 #include <chrono>
+#include <iostream>
+#include <string>
 #include <thread>
 #include <atomic>
 #include <future>
 using namespace std;
+
+// ANSI escape codes for color
+const string RED = "\033[31m";
+const string GREEN = "\033[32m";
+const string YELLOW = "\033[33m";
+const string RESET = "\033[0m";
 
 string generate_uuid()
 {
@@ -23,16 +28,14 @@ string generate_uuid()
 	return (uuid);
 }
 
-int	main() {
+void guess_missing_char() {
 	int score = 0;
-	while (true) 
-	{
+	while (true) {
 		vector<string> uuids(100);
 		generate(uuids.begin(), uuids.end(), generate_uuid);
 
 		char missing_char = "0123456789abcdef"[rand() % 16];
-		uuids.erase(remove_if(uuids.begin(), uuids.end(), [missing_char](const string& uuid) 
-		{
+		uuids.erase(remove_if(uuids.begin(), uuids.end(), [missing_char](const string& uuid) {
 			return uuid.find(missing_char) != string::npos;
 		}), uuids.end());
 
@@ -54,7 +57,7 @@ int	main() {
 
 		if (future.wait_for(chrono::seconds(30)) != future_status::ready) 
 		{
-			cout << "Time is up. Score counting is stopped" << endl;
+			cout << RED << "Time is up. Score counting is stopped" << RESET << endl;
 			cout << "Your final score is " << score << endl;
 			cout << "Thanks for trying the game. You can try the game by typing 'start'" << endl;
 			break;
@@ -65,19 +68,40 @@ int	main() {
 		chrono::duration<double> elapsed = end - start;
 		int remaining_time = 30 - static_cast<int>(elapsed.count());
 
+		cout << YELLOW << "Remaining time: " << remaining_time << " seconds\n" << RESET;
+
 		if (guess == missing_char)
 		{
 			score += remaining_time;
-			cout << "\nGood Work. The remaining time is being converted to score point(s) " << endl;
+			cout << GREEN << "\nGood Work. The remaining time is being converted to score point(s) " << RESET << endl;
 			cout << "Your are reworded with " << score << " score point(s) \n" << endl;
 		}
 		else
 		{
-			cout << "Wrong guess. The correct character was: " << missing_char << endl;
-			cout << "Unfortunately this the end\nFinal score is being calculated" << endl;
-			cout << "Your final score is: " << score << endl;
-			cout << "Thanks for trying the game. You can try the game by typing 'try again'\n" << endl;
+			cout << RED << "Wrong guess. The correct character was: " << missing_char << RESET << endl;
+			cout << RED << "Unfortunately this the end\nFinal score is being calculated" << RESET << endl;
+			cout << RED << "Your final score is: " << score << RESET << endl;
+			cout << GREEN << "Thanks for trying the game. You can try the game by typing 'start'\n" << RESET << endl;
 			break;
 		}
 	}
+}
+
+int main() {
+	string input;
+	while (true) 
+	{
+		cout << YELLOW << "Type 'start' to start the game" << RESET << endl;
+		cin >> input;
+		if (input == "start") 
+		{
+			cout << GREEN << "\t\t\t\tGame started.\n\t\tPlease try to guess correct missing character\n" << RESET;
+			guess_missing_char();
+		}
+		else 
+		{
+			cout << RED << "Invalid input. Please type 'start' to start the game" << RESET << endl;
+		}
+	}
+	return 0;
 }
