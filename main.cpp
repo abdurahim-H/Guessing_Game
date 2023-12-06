4f4118cfc21bc411e81089b29a60dc3911feb0cb
Abdurahim Hudulov <abul.hudul@gmail.com>
2023-12-06 07:22:26 +0100

    timer is fully fixed and code is perfeclty working

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 #include <chrono>
 #include <thread>
 #include <atomic>
+#include <future>
 using namespace std;
 
 string generate_uuid()
@@ -22,14 +23,15 @@ string generate_uuid()
 	return (uuid);
 }
 
-int main() {
+int	main() {
 	int score = 0;
-	while (true) {
+	while (true) 
+	{
 		vector<string> uuids(100);
 		generate(uuids.begin(), uuids.end(), generate_uuid);
 
 		char missing_char = "0123456789abcdef"[rand() % 16];
-		uuids.erase(remove_if(uuids.begin(), uuids.end(), [missing_char](const string& uuid)
+		uuids.erase(remove_if(uuids.begin(), uuids.end(), [missing_char](const string& uuid) 
 		{
 			return uuid.find(missing_char) != string::npos;
 		}), uuids.end());
@@ -43,9 +45,22 @@ int main() {
 
 		auto start = chrono::high_resolution_clock::now();
 
-		char guess;
-		cin >> guess;
+		auto future = async(launch::async, []() {
+			char guess;
+			cout << endl;
+			cin >> guess;
+			return guess;
+		});
 
+		if (future.wait_for(chrono::seconds(30)) != future_status::ready) 
+		{
+			cout << "Time is up. Score counting is stopped" << endl;
+			cout << "Your final score is " << score << endl;
+			cout << "Thanks for trying the game. You can try the game by typing 'start'" << endl;
+			break;
+		}
+
+		char guess = future.get();
 		auto end = chrono::high_resolution_clock::now();
 		chrono::duration<double> elapsed = end - start;
 		int remaining_time = 30 - static_cast<int>(elapsed.count());
@@ -53,13 +68,16 @@ int main() {
 		if (guess == missing_char)
 		{
 			score += remaining_time;
-			cout << "Correct" << score << "." << endl;
-		} 
+			cout << "\nGood Work. The remaining time is being converted to score point(s) " << endl;
+			cout << "Your are reworded with " << score << " score point(s) \n" << endl;
+		}
 		else
 		{
-			cout << "Incorrect" << score << "." <<endl;
+			cout << "Wrong guess. The correct character was: " << missing_char << endl;
+			cout << "Unfortunately this the end\nFinal score is being calculated" << endl;
+			cout << "Your final score is: " << score << endl;
+			cout << "Thanks for trying the game. You can try the game by typing 'try again'\n" << endl;
 			break;
 		}
 	}
-	return 0;
 }
